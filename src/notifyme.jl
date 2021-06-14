9d84625a72fc8374334e0c1dee4150071c22caa7
Aru Bhardwaj <aru.bhardwaj01@gmail.com>
2021-06-14 13:14:34 +0530

    Update notifyme.jl

--- src/notifyme.jl
+++ src/notifyme.jl
@@ -1,6 +1,6 @@
 
 function beep(sound_type::String)
-    basename = joinpath(@__DIR__, "..", "sounds", sound_type)
+    basename = joinpath(@__DIR__, "..", "sounds")
 
     path = joinpath(basename, string(sound_type, ".wav"))
     return(wavplay(path))
